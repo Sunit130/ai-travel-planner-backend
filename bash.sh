d36dc3ae40ca48e84f9b8f817919632649c1fe0c
sunit-iquanti <patwardhansunit@gmail.com>
2025-02-14 21:15:56 +0530

    TravelTales | Update deploy bash file

--- bash.sh
+++ bash.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+echo "Bash | Install requirements"
 pip install -r requirements.txt
 
+echo "Bash | Migrate"
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
+
+if [ "$CREATE_SUPERUSER" = "1"]; then
+  echo "Bash | Creating Super User"
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
+echo "Bash | Complete"
